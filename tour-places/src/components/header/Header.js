6e74ebd7d8c2fa333c88d8f0d8698e69c665da55
veyseltosun <postalarim1923@gmail.com>
2021-12-13 22:35:28 +0300

    with css

---
+++ tour-places/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import'./Header.css';
+
+
+const Header = () => {
+    return(
+        <div className="big-title">
+            <h3>POPULER TOUR PLACES</h3>
+        </div>
+    );
+};
+export default Header;
+
---
+++ tour-places/src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () =>{
+    return(
+        <div className='navbar'>
+            <a href='aboutus'>ABOUT US</a>
+            <a href='foryou'>FOR US</a>
+            <a href='servises'>SERVİCES</a>
+            <a href='blog'>BLOG</a>
+            <a href='contact'>CONTACT</a>
+        </div>
+    );
+    
+};
+
+
+
+
+export default Navbar;
